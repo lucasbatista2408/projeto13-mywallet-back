a26b0264f82c4a65cab37df62f29de54f570831f
lucasbatista <lucasbatista2408@gmail.com>
2022-06-30 15:28:38 -0300

    first commit

---
+++ database.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb"
+import dotenv from "dotenv"
+
+
+dotenv.config();
+
+const url = process.env.URL_DB_CONNECT;
+const client = new MongoClient(url);
+let db;
+
+try{
+    await client.connect()
+    db = client.db('mW_api')
+    console.log('conectado')
+} catch(err){
+    console.log(err)
+}
+
+export default db
---
+++ server.js
@@ -0,0 +1,54 @@
+import express from "express"
+import cors from "cors"
+import chalk from "chalk"
+import dotenv from "dotenv"
+import { MongoClient } from "mongodb"
+import db from "./database.js"
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+const PORT = process.env.PORT
+
+//HANDLES SIGN UP
+app.post('/signUp', async (req, res) => {
+    const newUser = req.body;
+    console.log(newUser);
+
+    try{
+        await db.collection('users').insertOne(newUser);
+        res.status(201).send('created')
+    } catch(err){
+        console.log(err)
+    }
+})
+
+app.get('/signUp', async (req, res) => {
+    const usersArr = await db.collection('users').find().toArray()
+    console.log(usersArr)
+    res.send(usersArr)
+})
+
+//HANDLES LOG IN
+app.post('/logIn', async (req, res) => {
+    const user = req.body;
+    console.log(user)
+    const found = await db.collection('users').findOne({user: user.user, password: user.password})
+    console.log(found)
+
+    if(!found){
+       return res.sendStatus(404)
+    } else{
+        return res.sendStatus(200)
+    }
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(chalk.bold.blue('SERVER_UP'));
+  });
